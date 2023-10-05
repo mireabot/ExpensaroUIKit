a9625aa48ec245943a911e7a92a43fc5bbb5856a
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2023-10-05 14:59:22 -0400

    Utilized list cells with one design

--- Sources/ExpensaroUIKit/Cells/EXCategoryCell.swift
+++ Sources/ExpensaroUIKit/Cells/EXCategoryCell.swift
@@ -2,7 +2,7 @@
 //  EXCategoryCell.swift
 //  
 //
-//  Created by Mikhail Kolkov on 9/18/23.
+//  Created by Mikhail Kolkov on 9/22/23.
 //
 
 import SwiftUI
@@ -10,26 +10,50 @@ import SwiftUI
 public struct EXCategoryCell: View {
   var icon: Image
   var title: String
+  var leftIcon: Image?
   
-  public init(icon: Image, title: String) {
+  /// Init for categoryCell
+  /// - Parameters:
+  ///   - icon: icon of category
+  ///   - title: name of category
+  ///   - leftIcon: optional left icon for selection
+  public init(icon: Image, title: String, leftIcon: Image? = nil) {
     self.icon = icon
     self.title = title
+    self.leftIcon = leftIcon
     MuktaFont.registerFonts()
   }
   
   public var body: some View {
-    VStack(spacing: 5) {
+    HStack {
       icon
-        .font(.title2)
         .foregroundColor(.primaryGreen)
+        .padding(12)
+        .background(Color.backgroundGrey)
+        .cornerRadius(12)
+      
       Text(title)
         .font(.mukta(.regular, size: 17))
-        .multilineTextAlignment(.center)
       
+      Spacer()
+      
+      leftIcon
+        .foregroundColor(.black)
     }
-    .padding(.vertical, 15)
-    .frame(maxWidth: .infinity, alignment: .center)
-    .background(Color.backgroundGrey)
-    .cornerRadius(12)
+    .background(Color.white)
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+  static var previews: some View {
+    List {
+      EXCategoryCell(icon: .init(systemName: "globe"), title: "Travel")
+      EXCategoryCell(icon: .init(systemName: "globe"), title: "Travel")
+      EXCategoryCell(icon: .init(systemName: "globe"), title: "Travel")
+      EXCategoryCell(icon: .init(systemName: "globe"), title: "Travel")
+    }
+    .listStyle(.plain)
+    .background(Color.white)
   }
 }
